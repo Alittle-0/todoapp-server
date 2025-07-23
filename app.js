e0662ef0264b3b186c17596198607898ceb61453
Alittle-0 <phihungdang1133@gmail.com>
2025-07-23 22:31:40 +0700

    fix

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const CORS = {
     origin: [
         "http://localhost:3000",
         "http://127.0.0.1:3000", 
-        "https://todoapp-pi-khaki.vercel.app"
+        process.env.FRONTEND_URL
     ],
     credentials: true,
     optionsSuccessStatus: 200,
